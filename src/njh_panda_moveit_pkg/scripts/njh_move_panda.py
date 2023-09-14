1ed14a84a902bf6eb55f5fef324427e1113b0313
NamJunHee <62279617+NamJunHee@users.noreply.github.com>
2023-09-14 14:38:39 +0900

    Update njh_move_panda.py

--- src/njh_panda_moveit_pkg/scripts/njh_move_panda.py
+++ src/njh_panda_moveit_pkg/scripts/njh_move_panda.py
@@ -79,9 +79,9 @@ def move_panda():
    #좌표 목표 계획 및 실행
    pose_goal = geometry_msgs.msg.Pose()
    pose_goal.orientation.w = 0.0
-   pose_goal.position.x = 0.1
+   pose_goal.position.x = 0.2
    pose_goal.position.y = 0.0
-   pose_goal.position.z = 0.4
+   pose_goal.position.z = 0.7
 
    # quaternion = tf.transformations.quaternion_from_euler(0, 0, 0, 'rzyx')
    # 회전 순서 y->z->x
